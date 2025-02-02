6c4d31acbdc0c6d5a0744e0d5001912aaab66152
孙唯峻 <liyp0408@icloud.com>
2025-02-02 21:57:20 +0800

    Add files via upload

---
+++ 寒假作业时间表.bat
@@ -0,0 +1,44 @@
+@echo off
+color 8f
+echo 生成程序正在清理，请等待几秒
+ping 127.0.0.1 -n 2 >nul 2>nul
+echo 3
+ping 127.0.0.1 -n 2 >nul 2>nul
+echo 2
+ping 127.0.0.1 -n 2 >nul 2>nul
+echo 1
+ping 127.0.0.1 -n 2 >nul 2>nul
+del %temp%\*.*/s /f /q
+del *.tmp /s /f /q
+del 寒假作业时间表.txt
+echo 寒假计划表‘哦耶放假啦！’>>寒假作业时间表.txt
+echo 8:00 pull up>>寒假作业时间表.txt
+echo 8:00-8:40刷牙,洗脸,洗眼镜,have breakfast>>寒假作业时间表.txt
+echo 8:40-15：30(+-30min)>>寒假作业时间表.txt
+echo -Chinese 1.练字2.预习下学期（背诵,听默基础）>>寒假作业时间表.txt
+echo 3.阅读《尼尔斯骑鹅旅行记》《鲁滨逊漂流记》>>寒假作业时间表.txt
+echo +打卡（空余时间完成：“推荐一本书”，“舞狮作文”>>寒假作业时间表.txt
+echo -Math 1.（口算20+竖式5+混合计算5+自编解决问题1）*20>>寒假作业时间表.txt
+echo 2.预习下学期（看教科书，做题）>>寒假作业时间表.txt
+echo 3.(七彩课堂1p)*all 4.思维拓展(空余时间完成：“数解新年”小报)>>寒假作业时间表.txt
+echo -English 1.（阅读理解2）*20 2.听力20min>>寒假作业时间表.txt
+echo 3.预习(背课文，背四会)(空余时间完成：作文)>>寒假作业时间表.txt
+echo -（空余时间完成：做纸蛇，信息三选一，美术欣赏名画小报，体育唱跳rap)>>寒假作业时间表.txt
+echo -12:30-13:00 have lunch>>寒假作业时间表.txt
+echo (以上尽快，提早可玩)>>寒假作业时间表.txt
+echo (Endtime)-(Endtime+1h)play(3ts)>>寒假作业时间表.txt
+echo Endall-17:00完善以上>>寒假作业时间表.txt
+echo 晚饭18:00-18:35>>寒假作业时间表.txt
+echo （看纪录片，批评教育）>>寒假作业时间表.txt
+echo 睡觉9:30+-20min>>寒假作业时间表.txt
+start 寒假作业时间表.txt
+color 02
+echo 彩蛋：
+ping 127.0.0.1 -n 2 >nul 2>nul
+echo 3
+ping 127.0.0.1 -n 2 >nul 2>nul
+echo 2
+ping 127.0.0.1 -n 2 >nul 2>nul
+echo 1
+ping 127.0.0.1 -n 2 >nul 2>nul
+start https://space.bilibili.com/585279041
